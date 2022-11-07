dda4b932ba65489079ba1c86fdff3b4e74aebe96
Mr. X <mr.x@Mrs-MacBook-Air.local>
2022-11-07 11:24:15 +0600

    asdd

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const cors = require('cors');
+// const { MongoClient, ServerApiVersion } = require('mongodb');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
+const jwt = require('jsonwebtoken');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -10,32 +12,80 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.twtll.mongodb.net/?retryWrites=true&w=majority`;
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.k5k5bzp.mongodb.net/?retryWrites=true&w=majority`;
+
+console.log(uri)
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+//verifying jwt token 
+function verifyJWT(req, res, next){
+    // next()
+    const authHeader = req.header.authorization;
+    if(!authHeader){
+        res.send({message: 'unauthorized access'})
+        res.status(401).send({message: 'unauthorized access'})
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function(err ,decoded){
+        if(err){
+            res.status(401).ssend({message: 'unauthorized access'})
+        }
+        req.decoded = decoded;
+        next();
+        })
+}
+
+
+//using asyncronous function to load data from the database
 async function run() {
     try {
-        const serviceCollection = client.db('geniusCar').collection('services');
-        const orderCollection = client.db('geniusCar').collection('orders');
+        //here the database collection that we are using is genius-car and the collection folder name is services
+        const serviceCollection = client.db('genius-car').collection('services');
+        const orderCollection = client.db('genius-car').collection('orders');
 
+        //we use post method to create the jwt token in the server
+        app.post('/jwt', (req, res) =>{
+            const user = req.body;
+            // console.log(user);
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1h'})
+            //sending the token as an objext the property is token and the value is also token
+            res.send({token});
+        })
+
+        //get methods
         app.get('/services', async (req, res) => {
-            const query = {}
-            const cursor = serviceCollection.find(query);
+            //finding all the data using {} empty object
+            const query = {} 
+            //getting the query request from server collection
+            const cursor = serviceCollection.find(query); 
+            //when we recieve the data at the final step we must await becuase we don't know when the database will return us the value
             const services = await cursor.toArray();
+            //getting services 
             res.send(services);
         });
 
         app.get('/services/:id', async (req, res) => {
-            const id = req.params.id;
-            const query = { _id: ObjectId(id) };
+            // const id = req.params.id;
+            // console.log(req);
+            const query = { _id: ObjectId(req.params.id) };
+            //findOne is used when we want only one data
             const service = await serviceCollection.findOne(query);
             res.send(service);
         });
 
 
-        // orders api
-        app.get('/orders', async (req, res) => {
+        // orders api get method
+        app.get('/orders', verifyJWT, async (req, res) => {
+            const decoded = req.decoded;
+            console.log('inside orders api', decoded);
+
+            if(decoded.email !== req.query.email){ 
+                res.send({message: 'unauthorized access'})
+            }
+
             let query = {};
 
             if (req.query.email) {
@@ -43,22 +93,25 @@ async function run() {
                     email: req.query.email
                 }
             }
-
             const cursor = orderCollection.find(query);
             const orders = await cursor.toArray();
-            res.send(orders);
+            res.send(orders); 
         });
 
-        app.post('/orders', async (req, res) => {
+        //post methods
+        app.post('/orders', verifyJWT, async (req, res) => {
             const order = req.body;
             const result = await orderCollection.insertOne(order);
             res.send(result);
         });
 
-        app.patch('/orders/:id', async (req, res) => {
+        //patch methods
+        app.patch('/orders/:id', verifyJWT, async (req, res) => {
             const id = req.params.id;
+            //sending a field name status to the body
             const status = req.body.status
             const query = { _id: ObjectId(id) }
+            //setting the status key value with status value from body
             const updatedDoc = {
                 $set:{
                     status: status
@@ -68,7 +121,8 @@ async function run() {
             res.send(result);
         })
 
-        app.delete('/orders/:id', async (req, res) => {
+        //delete methods
+        app.delete('/orders/:id',verifyJWT, async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
             const result = await orderCollection.deleteOne(query);
@@ -82,14 +136,14 @@ async function run() {
     }
 
 }
-
+//running the function
 run().catch(err => console.error(err));
 
-
+//testing the server if it's working 
 app.get('/', (req, res) => {
-    res.send('genius car server is running')
+    res.send('genius car server is running');
 })
-
+//listening to the port
 app.listen(port, () => {
     console.log(`Genius Car server running on ${port}`);
 })
